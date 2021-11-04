eab4d7ef5fa95ebc15cf9593ede36e768b94cc65
Peter <peterwangsb@gmail.com>
2021-11-04 14:09:13 -0700

    update-backend
    "dev": "nodemon ./src/server/index"
    npm run dev

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+const express = require('express'); 
+const router = express.Router();        
+const app = express(); 
+const cors = require('cors');                
+
+// base
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = process.env.PORT || 4000;    
+
+// allows CORS
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Resource-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "*");
+    console.log("requst url = " + req.url);
+	next();
+})
+
+//Test for Postman
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World' });   
+});
+
+app.listen(port, () => {
+    console.log('Magic happens on port ' + port)}
+);
