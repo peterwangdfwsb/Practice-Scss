ce1b538c9a224b53f531f5643fffdd5a7d6cbd21
Peter <peterwangsb@gmail.com>
2021-11-04 11:47:56 -0700

    create-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
-function App() {
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.Wrap}>
+      <div className={styles.Item}>
+        <div className={styles.Txt}>
+          <h2> CSS1 </h2>
+          <p> Text1 </p>
+
+        </div>
+      </div>
+
+
+      <div className={styles.Item}>
+        <div className={styles.Txt}>
+          <h2> CSS2 </h2>
+          <p> Text2 </p>
+
+        </div>
+      </div>
+
+
+      <div className={styles.Item}>
+        <div className={styles.Txt}>
+          <h2> CSS3 </h2>
+          <p> Text3 </p>
+
+        </div>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
