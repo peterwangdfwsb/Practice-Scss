6e29f1499381d348f4e56311315c0ba8464ebf9c
Peter <peterwangsb@gmail.com>
2021-12-16 13:06:37 -0800

    Hello Wprld

--- src/server/index.js
+++ src/server/index.js
@@ -24,5 +24,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Magic happens on port ' + port)}
+    //console.log('Magic happens on port ' + port)};
+    console.log('Hello World');
 );
